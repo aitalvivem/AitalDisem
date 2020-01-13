bcf74b08bb88652b2528f6baadbccecb1fd9d802
aitalvivem <52562995+aitalvivem@users.noreply.github.com>
2020-01-13 16:30:11 +0100

    Add files via upload

--- sec/create_db.php
+++ sec/create_db.php
@@ -1,8 +1,7 @@
 <?php
 
 /*
-Avant lancer ce script il faut créer la base pour cela il faut exécuter les scripts suivants sur le serveur MySQL :
-	- script_joc.sql
+Before runnig this script you need to create the database. To do so execute the script "sec/sql/script_joc.sql" on the MySQL server
 */
 
 echo 'début de la récupération et de l\'insertion des données dans la base : <br/>';
@@ -10,11 +9,11 @@ echo 'début de la récupération et de l\'insertion des données dans la base :
 require_once 'sec/biblio_utils.php';
 require_once('sec/config.php');
 
-// on instancie les classes Db_manager et Api_manager
+// intanciate Db_manager and Api_manager
 $api_manager = new Api_manager($api_url, $api_username, $api_password);
 $db_manager = new Db_manager($db_host, $db_dbname, $db_username, $db_password);
 
-// on insere les varietes
+// insert the varieties
 $db_manager->insereVar("auvern", "Q35359", "Avergnat", "Auvernhat", "Auvergnat");
 $db_manager->insereVar("gascon", "Q35735", "Gascon", "Gascon", "Gascon");
 $db_manager->insereVar("lemosin", "Q427614", "Limousin", "Lemosin", "Limousin");
@@ -22,11 +21,11 @@ $db_manager->insereVar("lengadoc", "Q942602", "Languedocien", "Lengadocian", "La
 $db_manager->insereVar("provenc", "Q241243", "Provençal", "Provençau", "Provençal");
 $db_manager->insereVar("vivaraup", "Q1649613", "Vivaro-Alpin", "Vivaroaupenc", "Vivaro-Alpine");
 			
-// on insere les catégories
+// insert the lexicals categories
 echo 'résultat de l\'insertion des catégories : <br/>';
 var_dump(insereCat($catcsv_path, $cat_csv_delimiter, $api_manager, $db_manager));
 
-// on crée un pdo pour récupérer les frequences
+// instanciate PDO to get the frequencies
 try{
 	$pdoFreq = new PDO('mysql:host='.$dbfreq_host.';dbname='.$dbfreq_dbname.';charset=utf8', $dbfreq_username, $dbfreq_password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
@@ -34,13 +33,13 @@ catch (exeption $e){
 	die('Erreur : ' . $e->getMessage());
 }
 
-// on insere les lexèmes
+// insert the lexemes
 echo 'résultat de l\'insertion des lexèmes : <br/>';
 var_dump(recupLex($inputscsv_path, $inputscsv_delimiter, $dataxml_path, $pdoFreq, $db_manager));
 
-// si il faut utiliser les traduction
+// if we need to use the translations too
 if($useTraduction){
-	// on crée un pdo pour récupérer les traductions
+	// instanciate PDO to get the translations
 	try{
 		$pdoTrad = new PDO('mysql:host='.$dbtrad_host.';dbname='.$dbtrad_dbname.';charset=utf8', $dbtrad_username, $dbtrad_password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}
@@ -48,15 +47,15 @@ if($useTraduction){
 		die('Erreur : ' . $e->getMessage());
 	}
 	
-	// on insere les traductions 
+	// insert the translations
 	echo 'résultat de l\'insertion des traductions : <br/>';
 	var_dump(recupTrad($pdoTrad, $db_manager));
 	
-	// on recherche des items et on les associent aux lexemes
+	// search for items and link them to the lexemes
 	echo 'résultat de l\'insertion des items : <br/>';
 	var_dump(insereItemCorres($api_manager, $db_manager, true));
 }else{
-	// on recherche des items et on les associent aux lexemes
+	// search for items and link them to the lexemes
 	echo 'résultat de l\'insertion des items : <br/>';
 	var_dump(insereItemCorres($api_manager, $db_manager, false));
 }
